3f55aaa0be86129143a087f2d2f81bf9cd9acf96
lupantano <88135040+lupantano@users.noreply.github.com>
2021-07-30 01:32:42 +0200

    Update extension.js
    clean code

--- extension.js
+++ extension.js
@@ -16,59 +16,61 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 const {St, Clutter} = imports.gi;
-const Gdk = imports.gi.Gdk
 const Main = imports.ui.main;
 
 const stripHeight = 36;
 const stripOpacity = 90;
+const stripColor = 'background-color : gold';
 
-let panelButton, panelButtonText, strip;
-
-function followMouse(){
-  let [mouse_x, mouse_y, mask] = global.get_pointer();
-  strip.y = (mouse_y - stripHeight/2);
-  return true;
-}
+let panelButton, panelButtonIcon, strip, pointerWatch;
 
 function enable() {
+  // Detect Screen
+  let pMonitor = Main.layoutManager.primaryMonitor;
+
+  // Add to Panel
   panelButton = new St.Bin({
     style_class : "panel-button"
   });
-  panelButtonText = new St.Label({
-    text : "[Reagind Strip]",
-    y_align: Clutter.ActorAlign.CENTER,
+  panelButtonIcon = new St.Icon({
+    icon_name: 'emblem-important',
+    icon_size: '24',
   });
-  panelButton.set_child(panelButtonText);
+  panelButton.set_child(panelButtonIcon);
+  Main.panel._rightBox.insert_child_at_index(panelButton, 1);
 
+  // Create Strip
   strip = new St.Widget({
-    style : 'background-color : gold',
+    style : stripColor,
+    opacity: stripOpacity,
     reactive : false,
     can_focus : false,
     track_hover : false,
-    opacity: stripOpacity,
-    width : (1.5 * 2560),
+    width: pMonitor.width,
     height : stripHeight,
-    x : 0,
-    y : 0,
   });
+  Main.uiGroup.add_child(strip);
 
+  // Follow mouse
   const PointerWatcher = imports.ui.pointerWatcher;
   let interval = 1000 / Clutter.get_default_frame_rate();
-  let pointerWatch = PointerWatcher.getPointerWatcher().addWatch(interval, (x, y) => {
+  pointerWatch = PointerWatcher.getPointerWatcher().addWatch(interval, (x, y) => {
         strip.y = (y - stripHeight/2);
         return true;
   });
-
-  Main.uiGroup.add_child(strip);
-  Main.panel._rightBox.insert_child_at_index(panelButton, 1);
 }
 
 function disable() {
   Main.panel._rightBox.remove_child(panelButton);
   Main.uiGroup.remove_child(strip);
 
-  pointerWatch.remove();
-  pointerWatch = null;
-  strip.destroy;
-  strip = null;
+  if (pointerWatch) {
+    pointerWatch.remove();
+    pointerWatch = null;
+  }
+
+  if (strip) {
+    strip.destroy;
+    strip = null;
+  }
 }
